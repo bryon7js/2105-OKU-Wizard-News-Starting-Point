ea7859f190b0abcec0fa676948461f2dcce844de
Bryon Shotton <bryon7js@gmail.com>
2021-08-31 17:04:23 -0500

    completed project

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const postBank = require("./postBank");
 
-app.use(morgan('dev'));
-app.use(express.static('public'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/", (req, res) => {
-  const posts = postBank.list()
+const PORT = 1337;
 
-
-
-  const html = 
-  `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-      ${posts.map(post => `
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>${post.title}
-            <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>`
-      ).join('')}
-    </div>
-  </body>
-</html>`;
-  res.send(html);
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
 });
-
 
 app.get('/posts/:id', (req, res) => {
   const id = req.params.id;
